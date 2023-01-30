9b71cbd7243813ddc21792aa9e21af058e9189d8
Dines <william.dines@dxc.com>
2023-01-30 16:37:56 -0600

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Menu from "./Menu";
+import Home from "./Home";
+import About from "./About";
+import Experience from "./Experience";
+
+export {
+    Menu,
+    Home,
+    About,
+    Experience
+}
